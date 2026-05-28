14633894baf8059bef5a048874d6457a0b903ee1
jin <jin1391.jeon@samsung.com>
2026-05-28 14:44:45 +0900

    [Green] 3s, 2s1b, 1s2b, 2s0b 등 경우에 수에 대한 구현 진행

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,32 @@
 public class Game {
-    public void guess(String guessnumber) {
+    public String question;
+
+    public GameResult guess(String guessnumber) {
+        int strike;
+        int ball;
+
+        checkInvalidGuessNumber(guessnumber);
+        if (guessnumber.equals(question)) {
+            return new GameResult(true, 3, 0);
+        } else {
+            strike = 0;
+            ball = 0;
+            for(int j = 0; j < guessnumber.length(); j++) {
+                char Gnumber = guessnumber.charAt(j);
+                for(int i = 0; i < question.length(); i++) {
+                    char Qnumber = question.charAt(i);
+                    if(i==j && Qnumber == Gnumber) {
+                        strike ++;
+                    }else if (Qnumber == Gnumber) {
+                        ball++;
+                    }
+                }
+            }
+            return new GameResult(false, strike, ball);
+        }
+    }
+
+    private static void checkInvalidGuessNumber(String guessnumber) {
         if (guessnumber == null) {
             throw new IllegalArgumentException();
         }
@@ -9,9 +36,15 @@ public class Game {
         }
 
         for (char number : guessnumber.toCharArray()) {
-            if (number < 0 || number > 9) {
+            if (number < '0' || number > '9') {
                 throw new IllegalArgumentException();
             }
+        }
+
+        if (guessnumber.charAt(0) == guessnumber.charAt(1) ||
+                guessnumber.charAt(0) == guessnumber.charAt(2) ||
+                guessnumber.charAt(1) == guessnumber.charAt(2)) {
+            throw new IllegalArgumentException();
         }
     }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -23,6 +23,7 @@ class GameTest {
         checkGuessNumber("12");
         checkGuessNumber("1234");
         checkGuessNumber("12s");
+        checkGuessNumber("112");
 
     }
 
@@ -34,5 +35,62 @@ class GameTest {
         }catch (IllegalArgumentException e) {
 
         }
+    }
+
+    @Test
+    void correctAllNumber() {
+        questionGenerator("123");
+        GameResult result = game.guess("123");
+
+        matchingNumber(result, true, 3, 0);
+    }
+
+    private void questionGenerator(String question) {
+        game.question = question;
+    }
+
+    private static void matchingNumber(GameResult result, boolean solved, int strike, int ball) {
+        Assertions.assertThat(result.solved).isEqualTo(solved);
+        Assertions.assertThat(result.strike).isEqualTo(strike);
+        Assertions.assertThat(result.ball).isEqualTo(ball);
+    }
+
+    @Test
+    void notCorrectAllNumber() {
+        questionGenerator("123");
+        GameResult result = game.guess("456");
+
+        matchingNumber(result, false, 0, 0);
+    }
+
+    @Test
+    void Sike2Ball0() {
+        questionGenerator("123");
+        GameResult result = game.guess("173");
+
+        matchingNumber(result, false, 2, 0);
+    }
+
+    @Test
+    void Sike1Ball1() {
+        questionGenerator("123");
+        GameResult result = game.guess("137");
+
+        matchingNumber(result, false, 1, 1);
+    }
+    @Test
+    void Sike1Ball2() {
+        questionGenerator("123");
+        GameResult result = game.guess("132");
+
+        matchingNumber(result, false, 1, 2);
+    }
+
+    @Test
+    void Sike0Ball3() {
+        questionGenerator("123");
+        GameResult result = game.guess("231");
+
+        matchingNumber(result, false, 0, 3);
     }
 }
