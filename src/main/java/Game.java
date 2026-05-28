1402f8e428fd73f32d88a5f5f0a99f80ff9c39fb
jin <jin1391.jeon@samsung.com>
2026-05-28 14:51:15 +0900

    [Refactorion] guess 함수 리팩토링 - 변수 변경 - else 제거

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,28 +2,31 @@ public class Game {
     public String question;
 
     public GameResult guess(String guessnumber) {
-        int strike;
-        int ball;
-
         checkInvalidGuessNumber(guessnumber);
         if (guessnumber.equals(question)) {
             return new GameResult(true, 3, 0);
-        } else {
-            strike = 0;
-            ball = 0;
-            for(int j = 0; j < guessnumber.length(); j++) {
-                char Gnumber = guessnumber.charAt(j);
-                for(int i = 0; i < question.length(); i++) {
-                    char Qnumber = question.charAt(i);
-                    if(i==j && Qnumber == Gnumber) {
-                        strike ++;
-                    }else if (Qnumber == Gnumber) {
-                        ball++;
-                    }
+        }
+
+        int strike = 0;
+        int ball = 0;
+
+        for (int gussIndex = 0; gussIndex < guessnumber.length(); gussIndex++) {
+            char guess = guessnumber.charAt(gussIndex);
+            for (int questionIndex = 0; questionIndex < question.length(); questionIndex++) {
+                char answer = question.charAt(questionIndex);
+
+                boolean sameNumber = guess == answer;
+                boolean samePosition = gussIndex == questionIndex;
+
+                if (sameNumber && samePosition) {
+                    strike++;
+                } else if (sameNumber) {
+                    ball++;
                 }
             }
-            return new GameResult(false, strike, ball);
         }
+        return new GameResult(false, strike, ball);
+
     }
 
     private static void checkInvalidGuessNumber(String guessnumber) {
