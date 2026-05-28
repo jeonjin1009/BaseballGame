5a2a0a8bc1c1e676581c2c0787aa848942e2a744
jin <jin1391.jeon@samsung.com>
2026-05-28 14:11:11 +0900

    [Green] 추측값 Invalid 일때

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,7 +1,17 @@
 public class Game {
-    public void guess(Object guessnumber) {
+    public void guess(String guessnumber) {
         if (guessnumber == null) {
             throw new IllegalArgumentException();
+        }
+
+        if (guessnumber.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        for(char number : guessnumber.toCharArray()) {
+            if(number <0 || number > 9) {
+                throw new IllegalArgumentException();
+            }
         }
     }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,23 +1,38 @@
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
 
+    Game game;
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
 
     @Test
     void createGame() {
-        Game game = new Game();
         assertNotNull(game);
     }
 
     @Test
-    void throwExceptioninputNull() {
-        Game game = new Game();
-        assertThrows(IllegalArgumentException.class, () -> {
-            game.guess(null);
-        });
+    void throwExceptioninputInvalid() {
+        checkGuessNumber(null);
+        checkGuessNumber("12");
+        checkGuessNumber("1234");
+        checkGuessNumber("12s");
 
+    }
+
+
+    private void checkGuessNumber(String guessNumber) {
+        try {
+            game.guess(guessNumber);
+            fail();
+        }catch (IllegalArgumentException e) {
+
+        }
     }
 }
